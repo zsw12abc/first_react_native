ae9d8977a8d65914b95efbc589a9a6a7e952ea1e
Shaowei <zsw22@vip.qq.com>
2017-12-18 12:41:52 +1100

    setting up the reducers

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ADD_PLACE, DELETE_PLACE, DESELECT_PLACE, SELECT_PLACE} from "../actions/actionTypes";
 
 const initialState = {
 	places: [],
@@ -7,6 +8,37 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
+		case ADD_PLACE:
+			return {
+				...state,
+				places: state.places.concat({
+					key: Math.random(),
+					name: action.placeName,
+					image: {
+						uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png',
+					}
+				}),
+			};
+		case DELETE_PLACE:
+			return {
+				...state,
+				places: state.places.filter(place => {
+					return place.key !== state.selectedPlace.key;
+				}),
+				selectedPlace: null
+			};
+		case SELECT_PLACE:
+			return {
+				...state,
+				selectedPlace: state.places.find(place => {
+					return place.key === action.placeKey;
+				})
+			};
+		case DESELECT_PLACE:
+			return {
+				...state,
+				selectedPlace: null,
+			};
 		default:
 			return state;
 	}
