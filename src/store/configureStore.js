2f60c29baf35c7136042927e1f96f7776cef446b
Shaowei <zsw22@vip.qq.com>
2017-12-18 20:38:53 +1100

    link library with xcode and add library react-native-vector-icons

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,18 @@
-import {combineReducers, createStore} from 'redux';
+import {combineReducers, compose, createStore} from 'redux';
 import placesReducer from "./reducers/places";
 
 const rootReducers = combineReducers({
 	places: placesReducer,
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+	composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
 
 const configureStore = () => {
-	return createStore(rootReducers);
+	return createStore(rootReducers, composeEnhancers());
 };
 
 export default configureStore;
