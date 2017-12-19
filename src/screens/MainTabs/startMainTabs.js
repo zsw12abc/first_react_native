46986e0e2285918c8a1cde70722c9a84055380e4
Shaowei <zsw22@vip.qq.com>
2017-12-19 20:15:39 +1100

    add tab screen

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,20 +1,32 @@
 import {Navigation} from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 const startTabs = () => {
-	Navigation.startTabBasedApp({
-		tabs: [
-			{
-				screen: 'awesome-places.FindPlaceScreen',
-				label: 'Find Place',
-				title: 'Find Place'
-			},
-			{
-				screen: 'awesome-places.SharePlaceScreen',
-				label: 'Share Place',
-				title: 'Share Place'
-			}
-		]
-	});
+	Promise.all([
+		Icon.getImageSource("md-map", 30),
+		Icon.getImageSource("ios-share-alt", 30)
+	]).then(source => {
+			Navigation.startTabBasedApp({
+				tabs: [
+					{
+						screen: 'awesome-places.FindPlaceScreen',
+						label: 'Find Place',
+						title: 'Find Place',
+						icon: source[0]
+					},
+					{
+						screen: 'awesome-places.SharePlaceScreen',
+						label: 'Share Place',
+						title: 'Share Place',
+						icon: source[1]
+					}
+				]
+			});
+		}
+	);
+
+
 };
 
 export default startTabs;
