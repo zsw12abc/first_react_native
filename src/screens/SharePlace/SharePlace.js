4673c88ebcafd6e61fc062563babc50ad8ba3807
Shaowei <zsw22@vip.qq.com>
2017-12-20 21:48:11 +1100

    editing the share place screen

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {Button, Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {connect} from 'react-redux';
-
-import PlaceInput from '../../components/PlaceInput/PlaceInput';
 import {addPlace} from "../../store/actions/index";
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import MainText from '../../components/UI/MainText/MainText';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import imagePlaceholder from '../../assets/zelda.jpg';
 
 
 class SharePlaceScreen extends Component {
@@ -28,12 +30,51 @@ class SharePlaceScreen extends Component {
 
 	render() {
 		return (
-			<View>
-				<PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-			</View>
+			<ScrollView>
+				<View style={styles.container}>
+					<MainText><HeadingText>Share a place with us!</HeadingText></MainText>
+					<View style={styles.placeholder}>
+						<Image source={imagePlaceholder} style={styles.previewImage}/>
+					</View>
+					<View style={styles.button}>
+						<Button title={"Pick Image"}/>
+					</View>
+					<View style={styles.placeholder}>
+						<Text>Map</Text>
+					</View>
+					<View style={styles.button}>
+						<Button title={"Locate me"}/>
+					</View>
+					<DefaultInput placeholder={"Place Name"}/>
+					<View style={styles.button}>
+						<Button title={"Share the Place!"}/>
+					</View>
+				</View>
+			</ScrollView>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center'
+	},
+	placeholder: {
+		borderWidth: 1,
+		borderColor: 'black',
+		backgroundColor: '#eee',
+		width: '80%',
+		height: 150,
+	},
+	button: {
+		margin: 8
+	},
+	previewImage: {
+		width: '100%',
+		height: '100%'
+	}
+});
 
 const mapDispatchToProps = dispatch => {
 	return {
