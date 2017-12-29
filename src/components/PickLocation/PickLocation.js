cf6f8a582e76912af35465669301bfafa4047545
Shaowei <zsw22@vip.qq.com>
2017-12-29 13:12:49 +1100

    add mapview to picklocation components

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, Dimensions, StyleSheet, View} from 'react-native';
+import MapView from 'react-native-maps';
 
 class PickLocation extends Component {
+	state = {
+		focusedLocation: {
+			latitude: -37.8146489,
+			longitude: 145.1205072,
+			latitudeDelta: 0.012,
+			longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.012,
+		}
+	};
+
 	render() {
 		return (
 			<View style={styles.container}>
-				<View style={styles.placeholder}>
-					<Text>Map</Text>
-				</View>
+				<MapView style={styles.map} initialRegion={this.state.focusedLocation}/>
 				<View style={styles.button}>
 					<Button title={"Locate me"} onPress={() => alert('pick location')}/>
 				</View>
@@ -21,12 +29,9 @@ const styles = StyleSheet.create({
 		width: '100%',
 		alignItems: 'center'
 	},
-	placeholder: {
-		borderWidth: 1,
-		borderColor: 'black',
-		backgroundColor: '#eee',
-		width: '80%',
-		height: 150,
+	map: {
+		width: '100%',
+		height: 250,
 	},
 	button: {
 		margin: 8
