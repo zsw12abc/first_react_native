c9c9473068854b0a86b294dbaa1bf7090bae2f19
Shaowei Zhang <zsw22@vip.qq.com>
2017-12-30 12:39:08 +1100

    store data in firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,19 +1,27 @@
-import {ADD_PLACE, DELETE_PLACE} from "./actionTypes";
+import {DELETE_PLACE} from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
-	return {
-		type: ADD_PLACE,
-		placeName: placeName,
-		location: location,
-		image: image
-	}
+    return dispatch => {
+        const placeData = {
+            name: placeName,
+            location: location
+        };
+        fetch('https://first-react-nati-1514367405118.firebaseio.com/places.json', {
+            method: 'POST',
+            body: JSON.stringify(placeData)
+        }).catch(err => console.log(err))
+            .then(res => res.json())
+            .then(parsedRes => {
+                console.log(parsedRes);
+            })
+    }
 };
 
 export const deletePlace = (key) => {
-	return {
-		type: DELETE_PLACE,
-		placeKey: key
-	}
+    return {
+        type: DELETE_PLACE,
+        placeKey: key
+    }
 };
 //
 // export const selectPlace = (key) => {
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,19 @@
-import {combineReducers, compose, createStore} from 'redux';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
 import placesReducer from "./reducers/places";
 
 const rootReducers = combineReducers({
-	places: placesReducer,
+    places: placesReducer,
 });
 
 let composeEnhancers = compose;
 
 if (__DEV__) {
-	composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 
 const configureStore = () => {
-	return createStore(rootReducers, composeEnhancers());
+    return createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)));
 };
 
 export default configureStore;
