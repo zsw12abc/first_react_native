717c271f29c7fd5029c11b69fe1900e4049d648b
Shaowei Zhang <zsw22@vip.qq.com>
2017-12-30 14:46:05 +1100

    storing the remaining data

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,10 +2,6 @@ import {DELETE_PLACE} from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
-        const placeData = {
-            name: placeName,
-            location: location
-        };
         fetch('https://us-central1-first-react-nati-1514367405118.cloudfunctions.net/storeImage', {
             method: 'POST',
             body: JSON.stringify({
@@ -14,12 +10,16 @@ export const addPlace = (placeName, location, image) => {
         }).catch(err => console.log(err))
             .then(res => res.json())
             .then(parsedRes => {
-                console.log(parsedRes);
-            });
-        fetch('https://first-react-nati-1514367405118.firebaseio.com/places.json', {
-            method: 'POST',
-            body: JSON.stringify(placeData)
-        }).catch(err => console.log(err))
+                const placeData = {
+                    name: placeName,
+                    location: location,
+                    image: parsedRes.imageURL
+                };
+                return fetch('https://first-react-nati-1514367405118.firebaseio.com/places.json', {
+                    method: 'POST',
+                    body: JSON.stringify(placeData)
+                })
+            }).catch(err => console.log(err))
             .then(res => res.json())
             .then(parsedRes => {
                 console.log(parsedRes);
