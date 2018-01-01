48ec2368a4d5176fd9d4025706dbae04df6cf421
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-02 10:37:00 +1100

    sign users up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,27 @@
-import {TRY_AUTH} from './actionTypes';
-
 export const tryAuth = (authData) => {
-	return {
-		type: TRY_AUTH,
-		authData: authData
-	};
+	return dispatch => {
+		dispatch(authSignup(authData));
+	}
 };
+
+export const authSignup = (authData) => {
+	return dispatch => {
+		fetch('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDPJWC318j4l8MtrWCNTOG2LCt3XmjX3uk', {
+			method: 'POST',
+			body: JSON.stringify({
+				email: authData.email,
+				password: authData.password,
+				returnSecureToken: true,
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).catch(err => {
+			console.log('error');
+			alert('Authentication failed, please try again!')
+		}).then(res => res.json())
+			.then(parsedRes => {
+				console.log(parsedRes);
+			})
+	}
+}
