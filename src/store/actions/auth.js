80ec71090acf5f8115161d079f7e7a3f1267b527
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-02 20:53:10 +1100

    clear the auth storage(AsyncStorage)

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -86,6 +86,9 @@ export const authGetToken = () => {
 				resolve(token);
 			}
 		});
+		promise.catch(err => {
+			dispatch(authClearStorage());
+		});
 		return promise;
 	}
 };
@@ -97,5 +100,12 @@ export const authAutoSignIn = () => {
 				startMainTabs()
 			})
 			.catch(err => console.log('Failed to fetch token!'))
+	}
+};
+
+export const authClearStorage = () => {
+	return dispatch => {
+		AsyncStorage.removeItem('ap:auth:token');
+		AsyncStorage.removeItem('ap:auth:expiryDate');
 	}
 };
