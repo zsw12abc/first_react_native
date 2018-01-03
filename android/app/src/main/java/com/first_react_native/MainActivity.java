4739220d147060ecb528004797c57177d2a27943
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-03 13:02:57 +1100

    add splashing screen

--- android/app/src/main/java/com/first_react_native/MainActivity.java
+++ android/app/src/main/java/com/first_react_native/MainActivity.java
@@ -1,7 +1,29 @@
 package com.first_react_native;
 
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new Linearlayout(this);
+        TextView textView = new TextView(this);
+
+        view.setBackgroundColor(Color.parseColor('#521751'));
+        view.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.parseColor('#fa923f'));
+        textView.setText('Awesome Places');
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+        view.addView(textView);
+        return view;
+    }
 
 }
